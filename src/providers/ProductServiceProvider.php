cfbc21db0ec1cdc9765bfdac01ee8eda8818bb54
nima <nima@webcodegenie.com>
2023-10-05 15:53:18 +0530

    update provider

--- src/providers/ProductServiceProvider.php
+++ src/providers/ProductServiceProvider.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Product\Category;
+namespace Wcg104\Product\Category;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Collection;
@@ -26,13 +26,13 @@ class ProductServiceProvider extends ServiceProvider
             ], 'product-category');
             $this->publishes([
               __DIR__.'/../Controllers/' => app_path('Http/Controllers')
-            ], 'lead');
+            ], 'product-category');
             $this->publishes([
               __DIR__.'/../Models/' => app_path('Models')
-            ], 'lead');
+            ], 'product-category');
             $this->publishes([
               __DIR__.'/../Requests' => app_path('Http/Requests')
-            ], 'lead');
+            ], 'product-category');
     }
 }
 
